40fa75a17fa7468c95136bc31d48f1370a10d8a3
Kauan B. Duarte <105992003+kbDuarte@users.noreply.github.com>
2026-09-09 20:13:15 -0300

    Adiciona configuração do app Android

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.catalogofilmes"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.catalogofilmes"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+}
