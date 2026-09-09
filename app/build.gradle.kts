6afafb0c56f60de5dbfa6c5dcda396f42fd1c127
Kauan B. Duarte <105992003+kbDuarte@users.noreply.github.com>
2026-09-09 20:46:15 -0300

    Ajusta SDK para compatibilidade com AGP 8.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.catalogofilmes"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.catalogofilmes"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
